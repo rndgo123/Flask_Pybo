7891ea4dff6092a610b74b03ec65e0b92ca3863d
sch <rndgo123@naver.com>
2021-01-04 18:27:28 +0900

    404 강제

--- config/production.py
+++ config/production.py
@@ -5,6 +5,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xe5\xb9\xecx-?EJ.\xe3\xa3\xf4\x12\x7f=\xac'
 
+'''
 dictConfig({
     'version': 1,
     'formatters': {
@@ -27,3 +28,4 @@ dictConfig({
         'handlers': ['file']
     }
 })
+'''
